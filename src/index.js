baeba1dd7cc41c2f82b43363e919d9de70619a00
Robert Rodriguez <robertorb21@gmail.com>
2017-07-03 18:55:03 -0500

    Adding interesting changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBDRIX7Lb6EIkm-BSAUPw-P9XglZtTNtTs';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //this.setState({ videos: videos })
+      // The last line is identica to this, it is by ES6
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
