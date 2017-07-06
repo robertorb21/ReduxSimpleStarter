8116d2409a471295209985b3a2047a7cc14ce8c4
Robert Rodriguez <robertorb21@gmail.com>
2017-07-06 15:37:58 -0500

    Adding video list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBDRIX7Lb6EIkm-BSAUPw-P9XglZtTNtTs';
 
 // Create a new component. This component should produce some HTML
@@ -11,7 +12,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'metallica'}, (videos) => {
       //this.setState({ videos: videos })
       // The last line is identica to this, it is by ES6
       this.setState({ videos });
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
