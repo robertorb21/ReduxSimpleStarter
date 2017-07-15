a82809c7778fc1c4575bd82d3ca31095fbe1d4c5
Robert Rodriguez <robertorb21@gmail.com>
2017-07-14 19:36:07 -0500

    Adding video list

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
-  return <li>MyVideo</li>
+//const VideoListItem = (props) => {
+//const video = props.video
+//esas dos lineas las podemos hacer una si le agregamos {} al parametro props y le ponemos direactamente la propiedad
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url
+  //console.log(video)
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  )
 }
 
 export default VideoListItem
